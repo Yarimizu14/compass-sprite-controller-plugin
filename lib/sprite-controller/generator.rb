d5a4f6da4928d532de941b7ba3fb7cdc02516e80
yarimizu.kensei <yarimizu.kensei14@gmail.com>
2014-03-13 21:04:41 +0900

    Magick::ImageクラスにSpriteChildをextend

--- lib/sprite-controller/generator.rb
+++ lib/sprite-controller/generator.rb
@@ -1,114 +1,19 @@
 require "compass"
 require "rmagick"
 
-
 module SpriteController
 
     module SpriteChild
-
-        attr_accessor :bgPos
-
-        def self.test
-            puts "child"
-        end
-
-        #module_function :test
+        attr_accessor :backgroundPos
     end
 
-end
-
-# 既存クラスの拡張
-module Magick
-    class Image
-
-        def test
-            puts "hoge"
-        end
-
-    end
-end
-
-module SpriteController
-
-    attr_accessor :spriteImg
-    attr_accessor :spriteImgChildren
-
-    #Magick::Image.extend(SpriteChild)
-
-    def createSprite
-        tmpImgList = readImgs
-=begin
-        # Mgick::ImageクラスのArrayに変換
-        @spriteImgChildren = tmpImgList.to_a
-
-        # スプライト画像を作成
-        @spriteImg = tmpImgList.append(true)
-
-        saveSprite
-=end
-
-=begin
-        # Arrayの長さとクラス名を取得
-        p @spriteImgChildren.length
-        p @spriteImgChildren[1].class
-
-        p @spriteImg.format
-        Compass::SpriteImporter::find_all_sprite_map_files("assets/icons/*.png")
-=end
-    end
-
-    def readImgs
-        p "test"
-        # 画像へのパスはコマンドを実行した場所からの相対パス
-        imgList = Magick::ImageList.new
-
-        path, name = Compass::SpriteImporter::path_and_name("assets/icons/*.png")
-        files = Compass::SpriteImporter::files("assets/icons/*.png")
-
-        files.each{|f|
-            imgList.read f
-        }
-
-        imgList.to_a.each{|f|
-            p f.class
-            f.test
-            #p f.bgPos
-            #f.bgPos = "hoge"
-        }
-
-        imgList
-    end
-
-
-    # スプライト画像を保存
-    def saveSprite
-        @spriteImg['comment'] = 'test'
-        @spriteImg.write("./images/sprite.png")
-    end
-
-    module_function :createSprite
-    module_function :readImgs
-    module_function :saveSprite
-
-
-    # 上となにが違う
-=begin
-    class SpriteChild < Magick::Image
-
-        attr_accessor :bgPos
-
-    end
-=end
-
-    class Sprite < Magick::Image
-
+    module Sprite
         attr_accessor :spriteImg
         attr_accessor :imgArray
         attr_accessor :imgList
         attr_accessor :spriteWidth, :spriteHeight
 
         def initialize()
-
         end
 
         def getPositionXByIndex()
@@ -135,5 +40,61 @@ module SpriteController
 
     end
 
+end
+
+
+module SpriteController
+
+    attr_accessor :spriteImg
+    attr_accessor :spriteImgChildren
+
+    def createSprite
+        tmpImgList = readImgs
+
+        # Mgick::ImageクラスのArrayに変換
+        @spriteImgChildren = tmpImgList.to_a
+
+        # スプライト画像を作成
+        @spriteImg = tmpImgList.append(true)
+
+        saveSprite
+
+=begin
+        # Arrayの長さとクラス名を取得
+        p @spriteImgChildren.length
+        p @spriteImgChildren[1].class
+
+        p @spriteImg.format
+        Compass::SpriteImporter::find_all_sprite_map_files("assets/icons/*.png")
+=end
+    end
+
+    def readImgs
+        # 画像へのパスはコマンドを実行した場所からの相対パス
+        imgList = Magick::ImageList.new
+
+        path, name = Compass::SpriteImporter::path_and_name("assets/icons/*.png")
+        files = Compass::SpriteImporter::files("assets/icons/*.png")
+
+        files.each{|f|
+            # ファイル名からMagick::Imageクラスのインスタンスを作成し、SpriteChildのメソッドを特異メソッド化
+            imgList.read(f).extend(SpriteController::SpriteChild)
+        }
+
+        imgList
+    end
+
+    # スプライト画像を保存
+    def saveSprite
+        @spriteImg['comment'] = 'test'
+        @spriteImg.write("./images/sprite.png")
+    end
+
+    module_function :createSprite
+    module_function :readImgs
+    module_function :saveSprite
 
 end
+
+
+
