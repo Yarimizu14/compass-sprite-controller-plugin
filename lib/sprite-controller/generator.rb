f516ef94f0d39a532996e8e98c45887129f9922f
yarimizu.kensei <yarimizu.kensei14@gmail.com>
2014-03-14 20:45:44 +0900

    縦方向に結合するスプライト画像生成メソッドを作成。background-positionを取得するメソッドを作成

--- lib/sprite-controller/generator.rb
+++ lib/sprite-controller/generator.rb
@@ -5,7 +5,8 @@ module SpriteController
 
     module SpriteChild
         # スプライト画像中でのbackground-position
-        attr_accessor :backgroundPos
+        attr_accessor :backgroundPos_x
+        attr_accessor :backgroundPos_y
     end
 
     module Sprite
@@ -21,7 +22,7 @@ module SpriteController
         # スプライト画像を保存
         def saveSprite
             self['comment'] = 'test'
-            self.write("./images/sprite.png")
+            self.write("./images/sample-1.png")
         end
 
     end
@@ -30,35 +31,74 @@ end
 
 module SpriteController
 
+    attr_accessor :debug
     attr_accessor :spriteImg
     attr_accessor :spriteImgChildren
 
+    class SpriteManager
+        attr_accessor :spriteImg
+    end
+
     def createSprite
+        p @spriteImgChildren.class
+
         readImgs
 
         # スプライト画像を作成
         @spriteImg = @spriteImgChildren.append(true).extend(SpriteController::Sprite)
+        @spriteImg = composeSprite.extend(SpriteController::Sprite)
+
+        @spriteImgChildren.to_a.each{|img|
+            #img.extend(SpriteController::SpriteChild)
+            p img.backgroundPos_y
+        }
 
         @spriteImg.saveSprite
     end
+
+    def background_pos()
+        @spriteImgChildren.to_a[1].backgroundPos_y
+    end
+
 
     def readImgs
         # 画像へのパスはコマンドを実行した場所からの相対パス
         @spriteImgChildren = Magick::ImageList.new
 
         path, name = Compass::SpriteImporter::path_and_name("assets/icons/*.png")
-        files = Compass::SpriteImporter::files("assets/icons/*.png")
+        files = Compass::SpriteImporter::files("assets/sample-1/*.png")
 
         files.each{|f|
             # ファイル名からMagick::Imageクラスのインスタンスを作成し、SpriteChildのメソッドを特異メソッド化
-            @spriteImgChildren.read(f).extend(SpriteController::SpriteChild)
+            @spriteImgChildren.read(f).to_a.each{|img|
+                img.extend(SpriteController::SpriteChild)
+            }
         }
 
         @spriteImgChildren
     end
 
+    def composeSprite
+
+        pos_y = 0
+        pos_x = 0
+        @spriteImgChildren.backgroundPos_y = pos_y
+
+        @spriteImgChildren.to_a.each{ |img|
+            pos_y += img.columns
+
+            img.backgroundPos_y = pos_y
+            img.backgroundPos_x = 0
+        }
+
+        @spriteImgChildren.append(true)
+
+    end
+
     module_function :createSprite
     module_function :readImgs
+    module_function :composeSprite
+    module_function :background_pos
 
 end
 
